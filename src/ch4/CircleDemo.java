07ff787ed6e63914dfc105d63aecf57557b0b5af
Jeon_gah <wjsrkgus0119@naver.com>
2024-08-15 17:22:22 +0900

    test2

--- src/ch4/CircleDemo.java
+++ src/ch4/CircleDemo.java
@@ -2,6 +2,24 @@ package ch4;
 
 public class CircleDemo {
   public static void main(String[] args) {
+    Circle c = new Circle();
+    c.setRadius(10.0);
+    c.show(c.getRadius(), c.findArea());
+  }
+}
 
+class Circle{
+  private double radius;
+  public double getRadius(){
+    return radius;
+  }
+  public void setRadius(double r){
+    this.radius = r;
+  }
+  double findArea(){
+    return 3.14 * radius * radius;
+  }
+  void show(double x, double y){
+    System.out.printf("반지름 = %.1f, 넓이 = %.1f\n", x, y);
   }
 }
