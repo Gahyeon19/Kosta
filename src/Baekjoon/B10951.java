eee6431e05092855b68e71291ac6b4000fb47d0c
Gahyeon <wjsrkgus0119@naver.com>
2024-08-31 23:33:18 +0900

    Scanner 방식

---
+++ src/Baekjoon/B10951.java
@@ -0,0 +1,17 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class B10951 {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    String[] s;
+    while (sc.hasNext()){
+      s = sc.nextLine().split(" ");
+      int a = Integer.parseInt(s[0]);
+      int b = Integer.parseInt(s[1]);
+      System.out.println(a + b);
+    }
+
+  }
+}
